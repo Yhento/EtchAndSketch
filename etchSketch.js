b8dfe29966c3ac4662497ead0d3fccb8a645a639
Tyler Payne <154254574+Yhento@users.noreply.github.com>
2024-03-04 09:57:58 -0600

    feat: create HTML page and createGrid function.

---
+++ etchSketch.js
@@ -0,0 +1,14 @@
+const createGrid = (gridSize=16) => {
+
+    grid_container = document.querySelector(".grid-container");
+
+    width_of_container = grid_container.offsetWidth;
+    
+    
+
+};
+
+createGrid();
+
+
+
